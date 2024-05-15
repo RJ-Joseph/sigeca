1cc1b7d6a47af6dc087f07aa11cee3d67623f59f
Ruano Jama Joseph Johan <joseph.ruano.jama@utelvt.edu.ec>
2024-05-15 09:09:49 -0500

    Update

--- application/views/canton_form.php
+++ application/views/canton_form.php
@@ -1,22 +1,34 @@
 <h2> <?php echo $title; ?> </h2>
 <hr/>
-<?php echo form_open("canton/save") ?>
-<?php echo form_hidden("idcanton")  ?>
-<table>
+<?php echo form_open("canton/save", array('id'=>'eys-form')); ?>
+<?php echo form_hidden("idcanton");  ?>
 
 
 
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Id canton:</label>
+	<div class="col-md-10">
+<?php echo form_input("idcanton","", array("placeholder"=>"Id canton"));  ?>
+	</div> 
+</div>
 
 
-<tr>
-<td> Nombre </td>
-<td><?php echo form_input("nombre","", array("placeholder"=>"Descripcion de canton"))  ?></td>
-</tr>
+<div class="form-group row">
+    <label class="col-md-2 col-form-label"> Nombre:</label>
+	<div class="col-md-10">
+<?php echo form_input("nombre","", array("placeholder"=>"Descripcion de canton"));  ?>
+	</div> 
+</div> 
 
-<tr>
-<td colspan="2"> <hr><?php echo form_submit("submit", "Guardar"); ?><?php echo anchor("canton","Atras") ?> </td>
-</tr>
+<div id="eys-nav-i">
 
-</table>
+	<ul>
+   	 	<li> <a href="javascript:{}" onclick="document.getElementById('eys-form').submit(); return false;">Guardar</a></li>
+    		<li> <?php echo anchor('persona', 'Cancelar'); ?></li>
+	</ul>
+</div>
+ 
+
+
 <?php echo form_close();?>
 
